8caaa9b25a688dc705fb5c72311fe175e212aad0
drake.huang <drake.huang@lollipop.camera>
2024-03-11 10:28:11 +0800

    InjectObjectTestable

--- PokmonTests/PokmonTests.swift
+++ PokmonTests/PokmonTests.swift
@@ -34,3 +34,27 @@ final class PokmonTests: XCTestCase {
     }
 
 }
+protocol InjectObjectTestable {
+    /// 多一個參數原因是希望多一點點小限制，顯示的讓呼叫的人傳遞介面進來
+    ///
+    ///  class Mock: AProtocol { }
+    ///  let a = Mock()
+    ///  sut.mock(dependency: a, type: AProtocol.self)
+    ///
+    ///  如果取消參數外面就要在其他地方顯式的宣告型別
+    ///  為了避免使用場景多出額外的隱式限制，粗暴地交給程序員。
+    ///  let a: AProtocol = Mock()
+    ///  sut.mock(dependency: a)
+    func mock<T>(dependency: T,_ type: T.Type)
+}
+
+extension InjectObjectTestable {
+    func mock<T>(dependency: T,_ type: T.Type) {
+        InjectObject.shared.container.register(T.self) { _ in
+            return dependency
+        }.inObjectScope(.container)
+    }
+}
+
+extension APIService: InjectObjectTestable { }
+extension UserDefaultWrapper: InjectObjectTestable { }
